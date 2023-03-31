fca00c933b03e08c99a13f106ecbb022c0929e18
iwamotomayumi <aokisantino.iwamayu@gmail.com>
2023-03-31 14:12:15 +0000

    文字列を結合

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert('Hello World');
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1 + str2);
+
+
+alert('Hello' + 'World');
+
+alert(10 / 2);
+
+alert(4 + 3);
+
+
+let int1 = 1;
+alert(int1);
+
+let hello = 'Hello World';
+alert(hello);
+
