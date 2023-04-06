37df267e5e1ebe9f2d4b5d1728cc905a41d042b0
iwamotomayumi <aokisantino.iwamayu@gmail.com>
2023-04-06 11:39:34 +0000

    for文

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
+
+
+// 引数の条件がtrueの間、波括弧内部の処理が繰り返される。
+//let max = 100;
+//let num = 1;
+//let count = 0;
+
+//while(num < max){
+//  num = num * 2;
+//  count = count + 1;
+//}
+
+//alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+// 条件1を満たすときは処理1、条件2を満たすときは処理2、どちらも満たさないときは処理3を実行
 let orange = 100;
 let apple = 120;
 
